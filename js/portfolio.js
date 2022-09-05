fda884c803ec2c42c2c3e5f20d88c5ea0e9836e1
Nohwongi <wonginoh@gmail.com>
2022-09-05 15:49:09 +0900

    portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,28 +10,114 @@ $(function(){
 
 $(document).ready(function($) {
     $(".scroll-home").click(function(event){      
-            event.preventDefault();
-            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        if(document.getElementById('nav_list').style.display === 'block') {
+            document.getElementById('nav_list').style.display = 'none';
+        };
+        event.preventDefault();
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     });
-    $(".scroll-aboutme").click(function(event){      
+    $(".scroll-aboutme").click(function(event){
+        if(document.getElementById('nav_list').style.display === 'block') {
+            document.getElementById('nav_list').style.display = 'none';
+        };
+        if($(window).width() > 735) {
             event.preventDefault();
             window.scrollTo({ top: 507, left: 0, behavior: 'smooth' });
+        } else {
+            event.preventDefault();
+            window.scrollTo({ top: 357, left: 0, behavior: 'smooth' });
+        };
     });
     $(".scroll-skills").click(function(event){      
+        if(document.getElementById('nav_list').style.display === 'block') {
+            document.getElementById('nav_list').style.display = 'none';
+        };
+        if($(window).width() > 767) {
             event.preventDefault();
             window.scrollTo({ top: 907, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 767 && $(window).width() > 735) {
+            event.preventDefault();
+            window.scrollTo({ top: 921, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 735 && $(window).width() > 510) {
+            event.preventDefault();
+            window.scrollTo({ top: 774, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 510 && $(window).width() > 390) {
+            event.preventDefault();
+            window.scrollTo({ top: 993, left: 0, behavior: 'smooth' });
+        } else {
+            event.preventDefault();
+            window.scrollTo({ top: 917, left: 0, behavior: 'smooth' });
+        }
     });
-    $(".scroll-timeline").click(function(event){      
+    $(".scroll-timeline").click(function(event){
+        if(document.getElementById('nav_list').style.display === 'block') {
+            document.getElementById('nav_list').style.display = 'none';
+        };
+        if($(window).width() > 767) {
             event.preventDefault();
             window.scrollTo({ top: 1814, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 767 && $(window).width() > 735) {
+            event.preventDefault();
+            window.scrollTo({ top: 1828, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 735 && $(window).width() > 510) {
+            event.preventDefault();
+            window.scrollTo({ top: 1681, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 510 && $(window).width() > 390) {
+            event.preventDefault();
+            window.scrollTo({ top: 1900, left: 0, behavior: 'smooth' });
+        } else {
+            event.preventDefault();
+            window.scrollTo({ top: 1824, left: 0, behavior: 'smooth' });
+        }
     });
-    $(".scroll-project").click(function(event){      
+    $(".scroll-project").click(function(event){
+        if(document.getElementById('nav_list').style.display === 'block') {
+            document.getElementById('nav_list').style.display = 'none';
+        };
+        if($(window).width() > 767) {
             event.preventDefault();
             window.scrollTo({ top: 2721, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 767 && $(window).width() > 735) {
+            event.preventDefault();
+            window.scrollTo({ top: 2735, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 735 && $(window).width() > 510) {
+            event.preventDefault();
+            window.scrollTo({ top: 2588, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 510 && $(window).width() > 390) {
+            event.preventDefault();
+            window.scrollTo({ top: 2807, left: 0, behavior: 'smooth' });
+        } else {
+            event.preventDefault();
+            window.scrollTo({ top: 2731, left: 0, behavior: 'smooth' });
+        }
     });
     $(".scroll-contact").click(function(event){      
+        if(document.getElementById('nav_list').style.display === 'block') {
+            document.getElementById('nav_list').style.display = 'none';
+        };
+        if($(window).width() > 767) {
             event.preventDefault();
             window.scrollTo({ top: 3628, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 767 && $(window).width() > 735) {
+            event.preventDefault();
+            window.scrollTo({ top: 3642, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 735 && $(window).width() > 510) {
+            event.preventDefault();
+            window.scrollTo({ top: 3495, left: 0, behavior: 'smooth' });
+        } else if($(window).width() <= 510 && $(window).width() > 390) {
+            event.preventDefault();
+            window.scrollTo({ top: 3714, left: 0, behavior: 'smooth' });
+        } else {
+            event.preventDefault();
+            window.scrollTo({ top: 3638, left: 0, behavior: 'smooth' });
+        }
     });
 });
 
+function OpenCloseNav() {
+        if(document.getElementById('nav_list').style.display === 'block') {
+                document.getElementById('nav_list').style.display = 'none';
+        } else {
+                document.getElementById('nav_list').style.display = 'block';
+        };
+};
